a83d595fb9d63c3aa8d886e67aec5b957229a921
anadjurovic97 <anadjurovic@Anas-MacBook-Air.local>
2023-01-13 15:01:56 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'beaa633c-7bb8-4003-a7c3-20bbd3442f90';
+export const MODAL_CLOSE_SEC = 2.5;
